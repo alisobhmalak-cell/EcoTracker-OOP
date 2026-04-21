6393d280bcc0133e1a1fca4b9051f5d5fb8a8dc7
Hagar Ismail <hagerismail226@gmail.com>
2026-04-21 21:55:48 +0200

    Create Electricity.cs

---
+++ EcoTracker-OOP/Models/Electricity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic; 
+using System.Text;
+
+namespace CarponOS;
+
+    public class Electricity : EmissionActivity
+    {
+    // خصائص الجهاز(propertise)
+    public string DeviceName { get; set; }
+    public double ConsumptionRate { get; set; }
+    public double UsageHours { get; set; }
+    public double Emission(int Days)   // دالة لحساب الانبعاثات بناءً علي عدد الايام
+    {
+        return ConsumptionRate * UsageHours * Days * 0.5;
+
+    }
+}
+
+
+
