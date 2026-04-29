09eb78bb0441c3c4805ab3549eb9e83d1e1a4289
nada alaa <nadalaa.2007@gmail.com>
2026-04-29 05:29:25 +0300

    log in details

--- EcoTracker-OOP/Program.cs
+++ EcoTracker-OOP/Program.cs
@@ -16,7 +16,7 @@ namespace EcoTracker_OOP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new log_in_details());
         }
     }
 }
---
+++ EcoTracker-OOP/success log in.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EcoTracker_OOP
+{
+    public partial class success_log_in : Form
+    {
+        public success_log_in()
+        {
+            InitializeComponent();
+        }
+    }
+}
